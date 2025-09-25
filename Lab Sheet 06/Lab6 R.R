d8cf816677ec727111382463b41d9bb80884a26a
IT24100026 <it24100026@my.sliit.lk>
2025-09-25 10:44:03 +0530

    Add files via upload

---
+++ Lab Sheet 06/Lab6 R.R
@@ -0,0 +1,10 @@
+getwd()
+setwd("C:\\Users\\MSI-SWORD\\Desktop\\IT24100026_Lab6")
+#Q1
+#Binomal distribution
+pbinom(46, 50, 0.85,lower.tail = FALSE)
+
+#Q2
+#Number of calls received in given day
+#poisson distribution
+dpois(15, 12)
